0c2e0c508626fbbb2cc216571d47b6a7fff01c2b
zhangxu <391324751@qq.com>
2016-11-17 10:09:00 +0800

    add ignore

--- app/src/main/java/com/best/android/loler/adapter/OnItemClickListener.java
+++
@@ -1,8 +0,0 @@
-package com.best.android.loler.adapter;
-
-/**
- * Created by BL06249 on 2015/12/2.
- */
-public interface OnItemClickListener {
-    public void onItemClick(int position);
-}
--- app/src/main/java/com/best/android/loler/util/ScreenUtil.java
+++
@@ -1,8 +0,0 @@
-package com.best.android.loler.util;
-
-/**
- * Created by BL06249 on 2015/11/23.
- */
-public class ScreenUtil {
-
-}
