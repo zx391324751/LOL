3a68cbd7897bd500fb647239512aa6be08d23b22
zhangxu <391324751@qq.com>
2016-11-23 11:47:27 +0800

    斗鱼tv界面修改

--- app/src/main/java/com/best/android/loler/activity/DouyuActivity.java
+++ app/src/main/java/com/best/android/loler/activity/DouyuActivity.java
@@ -10,6 +10,7 @@ import android.widget.Toast;
 
 import com.best.android.loler.R;
 import com.best.android.loler.adapter.GroupListAdapter;
+import com.best.android.loler.config.Constants;
 import com.best.android.loler.http.BaseHttpService;
 import com.best.android.loler.http.GetRoomGroupService;
 import com.best.android.loler.model.GroupInfo;
@@ -22,36 +23,31 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 /**
  * Created by BL06249 on 2016/1/11.
  */
-public class DouyuActivity extends AppCompatActivity {
+public class DouyuActivity extends LoLBaseActivity {
 
+    @BindView(R.id.activity_douyu_rv)
     RecyclerView recyclerView;
     List<GroupInfo>listRooms;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void initView(Bundle savedInstanceState) {
         setContentView(R.layout.activity_douyu);
-
+        setTitle("斗鱼TV");
+        ButterKnife.bind(this);
 
         GetRoomGroupService getRoomGroupService = new GetRoomGroupService(this);
         getRoomGroupService.send(listener);
-
-        initView();
+        initViews();
     }
 
-    private void initView() {
-        recyclerView = (RecyclerView)findViewById(R.id.activity_douyu_rv);
+    private void initViews() {
         recyclerView.setLayoutManager(new GridLayoutManager(this, 3));
-
-        findViewById(R.id.activity_douyu_iv_back).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                DouyuActivity.this.finish();
-            }
-        });
     }
 
     BaseHttpService.ResponseListener listener = new BaseHttpService.ResponseListener() {
@@ -67,7 +63,7 @@ public class DouyuActivity extends AppCompatActivity {
 
         @Override
         public void onFail(String errorMsg) {
-            Toast.makeText(DouyuActivity.this, "数据加载失败", Toast.LENGTH_SHORT).show();
+            Toast.makeText(DouyuActivity.this, Constants.QUERY_ERROR, Toast.LENGTH_SHORT).show();
         }
     };
 
